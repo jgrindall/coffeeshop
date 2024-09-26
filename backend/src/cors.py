da0f8a77e6398bd8169682f2b0ffb329078e6cb2
john@2simple.com <john@2simple.com>
2024-09-26 20:16:39 +0100

    add patch

--- backend/src/cors.py
+++ backend/src/cors.py
@@ -7,6 +7,6 @@ def cors(app):
             "Access-Control-Allow-Headers", "Content-Type,Authorization,true"
         )
         response.headers.add(
-            "Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS"
+            "Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS,PATCH"
         )
         return response
